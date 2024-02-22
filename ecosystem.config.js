cde8eaa940604f694dfcf87463b0b57570bbbc50
huangbh <578092215@qq.com>
2024-02-22 13:44:50 +0800

    :bug: fix(custom): 调整部署脚本

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
       name: 'blog-server-v2',
       port: '7001',
       exec_mode: 'cluster',
-      instances: 'max',
       script: './bootstrap.js',
       env: {
         NODE_ENV: 'production',
